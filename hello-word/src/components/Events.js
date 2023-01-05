9dc2f4846f77a138ef51d3337bda17c5988f05b3
gillsonpassos <oliveirapassos07@outlook.com>
2023-01-05 16:05:38 -0300

    Funções nos Eventos

--- hello-word/src/components/Events.js
+++ hello-word/src/components/Events.js
@@ -7,6 +7,22 @@ const Events = () => {
       <div>
         <button onClick={handleMyEvent}>Click aqui!</button>
       </div>
+      <div>
+        <button onClick={() => console.log("Clicou!")}>
+          Click aqui tambem
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            if (true) {
+              console.log("isso não deveria existir =)");
+            }
+          }}
+        >
+          clique aqui por favor
+        </button>
+      </div>
     </div>
   );
 };
