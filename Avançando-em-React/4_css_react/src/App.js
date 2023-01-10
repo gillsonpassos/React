dc08d9bf1608d18907ecea0793f370d07bc8c68d
gillsonpassos <oliveirapassos07@outlook.com>
2023-01-10 17:36:00 -0300

    Css de componentes

--- Avançando-em-React/4_css_react/src/App.js
+++ Avançando-em-React/4_css_react/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import MyComponent from "./components/MyComponent";
 
 function App() {
   return (
     <div className="App">
+      {/*CSS GLOBAL */}
       <h1>React com CSS</h1>
+      {/*CSS COMPONENTE */}
+      <MyComponent />
+      <p>Este paragrafo é do App.js</p>
     </div>
   );
 }
