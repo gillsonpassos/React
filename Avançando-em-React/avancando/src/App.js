d1ff1f46523115e276781fe4d161dd9f0a29790c
gillsonpassos <oliveirapassos07@outlook.com>
2023-01-10 14:43:53 -0300

    Exercicios de Componentes e recursos do react

--- Avançando-em-React/avancando/src/App.js
+++ Avançando-em-React/avancando/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-
+import { useState } from "react";
 import City from "./assets/city.jpg";
 import CarDetails from "./components/CarDetails";
 import ConditionalRender from "./components/ConditionalRender";
@@ -9,11 +9,26 @@ import ShowUserName from "./components/ShowUserName";
 import Fragment from "./components/Fragment";
 import Container from "./components/Container";
 import ExecuteFunction from "./components/ExecuteFunction";
+import Message from "./components/Message";
+import ChangeMessageState from "./components/ChangeMessageState";
+import UserDetails from "./components/UserDetails";
 
 function App() {
   function showMessage() {
     console.log("Evento do Componente Pai!");
   }
+
+  const [message, setMessage] = useState("");
+
+  const handleMessage = (msg) => {
+    setMessage(msg);
+  };
+
+  const users = [
+    { id: 1, name: "Gilson", job: "Programador", age: 56 },
+    { id: 2, name: "Arly", job: "do lar", age: 52 },
+    { id: 3, name: "Alice", job: "estudante", age: 5 },
+  ];
 
   return (
     <div className="App">
@@ -48,6 +63,18 @@ function App() {
 
       {/*Executar Função */}
       <ExecuteFunction myFunction={showMessage} />
+
+      {/* state lift */}
+      <Message msg={message} />
+      <ChangeMessageState handleMessage={handleMessage} />
+      {users.map((user) => (
+        <UserDetails
+          key={user.id}
+          name={user.name}
+          job={user.job}
+          age={user.age}
+        />
+      ))}
     </div>
   );
 }
---
+++ Avançando-em-React/avancando/src/components/Message.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Message = ({ msg }) => {
+  return <div>A menssagem é {msg}</div>;
+};
+
+export default Message;
