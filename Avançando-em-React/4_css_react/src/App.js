6bf701848a2595046361e235e5e4f56ce6961fe6
gillsonpassos <oliveirapassos07@outlook.com>
2023-01-11 17:35:45 -0300

    Classes-Dinamicas

--- Avançando-em-React/4_css_react/src/App.js
+++ Avançando-em-React/4_css_react/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 import MyComponent from "./components/MyComponent";
 
+import { useState } from "react";
+
 function App() {
+  const n = 15;
+  const [name] = useState("GILSON");
+
+  const redTitle = false;
+
   return (
     <div className="App">
       {/*CSS GLOBAL */}
@@ -28,6 +35,26 @@ function App() {
       >
         Este elemento foi estilizado de forma Inline
       </p>
+
+      {/* CSS Inline dinamico*/}
+      <h2 style={n < 10 ? { color: "purple" } : { color: "pink" }}>
+        CSS dinamico
+      </h2>
+      <h2 style={n > 10 ? { color: "purple" } : { color: "pink" }}>
+        CSS dinamico
+      </h2>
+      <h2
+        style={
+          name === "GILSON" ? { color: "green", backgroundColor: "#000" } : null
+        }
+      >
+        Teste Nome
+      </h2>
+      {/* classe dinamica */}
+
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Este titulo vai ter classe dinamica
+      </h2>
     </div>
   );
 }
