1cf82af25bcce0db5b871260f226edb00bc76cdd
gillsonpassos <oliveirapassos07@outlook.com>
2023-01-12 16:32:25 -0300

    Criando Projeto

---
+++ Avançando-em-React/6_secret_word/secretword/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Retrovisor"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Abacate"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React", "Typescript"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+};
