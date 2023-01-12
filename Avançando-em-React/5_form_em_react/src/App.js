09e695dd8133e02cd6a074e2caf91ec035e9da9e
gillsonpassos <oliveirapassos07@outlook.com>
2023-01-12 09:36:31 -0300

    Criando Formulários

---
+++ Avançando-em-React/5_form_em_react/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Myform from "./components/Myform";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Forms</h2>
+      <Myform />
+    </div>
+  );
+}
+
+export default App;
