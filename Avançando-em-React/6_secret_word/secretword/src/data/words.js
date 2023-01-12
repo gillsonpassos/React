9623b6db2520c46be0eb5431b645ff6e745bd254
gillsonpassos <oliveirapassos07@outlook.com>
2023-01-12 17:29:05 -0300

    Finalizando tela Inicial

--- Avançando-em-React/6_secret_word/secretword/src/data/words.js
+++ Avançando-em-React/6_secret_word/secretword/src/data/words.js
@@ -1,7 +1,7 @@
 export const wordsList = {
   carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Retrovisor"],
   fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Abacate"],
-  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Nariz"],
   computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
   programação: ["Linguagem", "Framework", "JavaScript", "React", "Typescript"],
   alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
