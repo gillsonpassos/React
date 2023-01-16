ff2502afbbc93fa6f2055fcffa0c1a6f86d3db9b
gillsonpassos <oliveirapassos07@outlook.com>
2023-01-16 09:05:48 -0300

    Carregamento de dados dinamico

--- Avançando-em-React/7_REQ_HTTP/httpreact/src/App.js
+++ Avançando-em-React/7_REQ_HTTP/httpreact/src/App.js
@@ -38,6 +38,14 @@ function App() {
       },
       body: JSON.stringify(product),
     });
+
+    // 3 - carregamento  dinamico
+    const addedProduct = await res.json();
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    setName("");
+    setPrice("");
   };
 
   return (
