f7ff2f2b43565539829086e4757563dd36e2e690
gillsonpassos <oliveirapassos07@outlook.com>
2023-01-16 10:04:25 -0300

    custom hook para resgate de dados

--- Avançando-em-React/7_REQ_HTTP/httpreact/src/App.js
+++ Avançando-em-React/7_REQ_HTTP/httpreact/src/App.js
@@ -2,25 +2,31 @@ import "./App.css";
 
 import { useState, useEffect } from "react";
 
+// 4 - custom hook
+import { useFetch } from "./hooks/useFetch";
+
 const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
 
+  // 4 - custom
+  const { data: items } = useFetch(url);
+
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
 
   // 1 - Resgatando dados
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(url);
+  //useEffect(() => {
+  // async function fetchData() {
+  //  const res = await fetch(url);
 
-      const data = await res.json();
+  //  const data = await res.json();
 
-      setProducts(data);
-    }
-    fetchData();
-  }, []);
+  //  setProducts(data);
+  //  }
+  //  fetchData();
+  // }, []);
 
   // 2 - add de produtos
   const handleSubmit = async (e) => {
@@ -52,11 +58,12 @@ function App() {
     <div className="App">
       <h1>Lista de Produtos</h1>
       <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            {product.name} - R$: {product.price}
-          </li>
-        ))}
+        {items &&
+          items.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}
+            </li>
+          ))}
       </ul>
       <div className="add-product">
         <form onSubmit={handleSubmit}>
