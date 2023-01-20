72c3f8f265df5a6842a7f02beae85a24ad9a8214
gillsonpassos <oliveirapassos07@outlook.com>
2023-01-20 08:02:34 -0300

    Refatorando-contexto para hook

--- Avançando-em-React/9_react_context/context/src/pages/Home.js
+++ Avançando-em-React/9_react_context/context/src/pages/Home.js
@@ -1,9 +1,13 @@
-import { useContext } from "react";
+//import { useContext } from "react";
 import ChangeCounter from "../components/ChangeCounter";
-import { CounterContext } from "../context/CounterContext";
+//import { CounterContext } from "../context/CounterContext";
+
+// 4 - refactorando hook
+import { useCounterContext } from "../hooks/useCounterContext";
 
 const Home = () => {
-  const { counter } = useContext(CounterContext);
+  // const { counter } = useContext(CounterContext);
+  const { counter } = useCounterContext();
 
   return (
     <div>
