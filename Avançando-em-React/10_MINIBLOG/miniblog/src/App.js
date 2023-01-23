7562a6a71e49ffaa79a9d5c30ecb73631a38caa3
gillsonpassos <oliveirapassos07@outlook.com>
2023-01-23 16:35:28 -0300

    Criando formulário de cadastro

--- Avançando-em-React/10_MINIBLOG/miniblog/src/App.js
+++ Avançando-em-React/10_MINIBLOG/miniblog/src/App.js
@@ -7,6 +7,8 @@ import About from "./pages/About/About";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
@@ -15,8 +17,10 @@ function App() {
         <Navbar />
         <div className="container">
           <Routes>
-            <Route path="/about" element={<About />} />
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
         <Footer />
