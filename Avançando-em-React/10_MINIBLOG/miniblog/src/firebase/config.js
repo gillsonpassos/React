a238473cec8a192a187147d99e6a5da268bb805a
gillsonpassos <oliveirapassos07@outlook.com>
2023-01-28 10:06:06 -0300

    Registrando usuário no sistema

--- Avançando-em-React/10_MINIBLOG/miniblog/src/firebase/config.js
+++ Avançando-em-React/10_MINIBLOG/miniblog/src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firebase";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCXZ4SU7FW8eiw9zNrWjOK8s3Nx_232zZI",
