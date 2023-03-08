2e6911b0bf97c83c6c2a06e3cd7e0d13ed46b14f
gillsonpassos <oliveirapassos07@outlook.com>
2023-03-08 16:28:15 -0300

    useRedducer com actions

--- Avançando-em-React/11_HOOKS/reacthooks/src/components/HookUseReducer.js
+++ Avançando-em-React/11_HOOKS/reacthooks/src/components/HookUseReducer.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 const HookUseReducer = () => {
   // 1 Começando com useReducer
@@ -6,11 +6,63 @@ const HookUseReducer = () => {
     return Math.random(state);
   });
 
+  // 2 - avançando useReducer
+  const initialTasks = [
+    { id: 1, text: "Fazer alguma coisa" },
+    { id: 2, text: "Fazer outra coisa" },
+  ];
+
+  const taskReducer = (state, action) => {
+    // eslint-disable-next-line default-case
+    switch (action.type) {
+      case "ADD":
+        const newTask = {
+          id: Math.random(),
+          text: taskText,
+        };
+
+        setTaskText("");
+
+        return [...state, newTask];
+      case "DELETE":
+        return state.filter((task) => task.id !== action.id);
+      default:
+        return state;
+    }
+  };
+
+  const [taskText, setTaskText] = useState("");
+  const [tasks, dispatchTasks] = useReducer(taskReducer, initialTasks);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatchTasks({ type: "ADD" });
+  };
+
+  const removeTask = (id) => {
+    dispatchTasks({ type: "DELETE", id });
+  };
+
   return (
     <div>
       <h2>useReducer</h2>
       <p>Numero: {number}</p>
       <button onClick={dispatch}>Aterar Numero!</button>
+      <h3>Tarefas:</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTaskText(e.target.value)}
+          value={taskText}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+      {tasks.map((task) => (
+        <li key={task.id} onDoubleClick={() => removeTask(task.id)}>
+          {task.text}
+        </li>
+      ))}
       <hr />
     </div>
   );
