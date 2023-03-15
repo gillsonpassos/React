16811e323ae7396da3d1b5432dbc1932ef5e93a0
gillsonpassos <oliveirapassos07@outlook.com>
2023-03-15 10:11:20 -0300

    HookUseContext

--- Avançando-em-React/11_HOOKS/reacthooks/src/pages/Home.js
+++ Avançando-em-React/11_HOOKS/reacthooks/src/pages/Home.js
@@ -3,12 +3,21 @@ import HookUseEffect from "../components/HookUseEffect";
 import HookUseReducer from "../components/HookUseReducer";
 import HookUseState from "../components/HookUseState";
 
+// useContext
+import { useContext } from "react";
+import { SomeContext } from "../components/HookUseContext";
+
 const Home = () => {
+  const { contextValue } = useContext(SomeContext);
+
   return (
     <div>
       <HookUseState />
       <HookUseReducer />
       <HookUseEffect />
+      <h2>useContex</h2>
+      <p>valor do context: {contextValue}</p>
+      <hr />
     </div>
   );
 };
