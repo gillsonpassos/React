5fd9c6e127ff85f306c22efec94ce4c0472f33ba
gillsonpassos <oliveirapassos07@outlook.com>
2023-03-24 10:52:16 -0300

    useDebugValue

--- Avançando-em-React/11_HOOKS/reacthooks/src/hooks/usePrevious.js
+++ Avançando-em-React/11_HOOKS/reacthooks/src/hooks/usePrevious.js
@@ -1,7 +1,10 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useDebugValue } from "react";
 
 export const usePrevious = (value) => {
   const ref = useRef;
+
+  useDebugValue("---custom Hook e USEDEBUGVALUE----");
+  useDebugValue("O numero anterior é:" + value);
 
   useEffect(() => {
     ref.current = value;
