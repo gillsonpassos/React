ca20c717b70509826e5bf24f4e70b9cad14bb220
gillsonpassos <oliveirapassos07@outlook.com>
2023-04-05 10:24:26 -0300

    Registro do Usuario

--- Avançando-em-React/12_REACTGRAM/backend/controllers/UserController.js
+++ Avançando-em-React/12_REACTGRAM/backend/controllers/UserController.js
@@ -14,7 +14,39 @@ const generateToken = (id) => {
 
 // Register user and sign in
 const register = async (req, res) => {
-  res.send("Registro");
+  const { name, email, password } = req.body;
+
+  // check if user exists
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(422).json({ errors: ["Por favor, ultilize outro e-mail"] });
+    return;
+  }
+
+  // Generate password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  // Creat User
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  // if user was created successfully, return the token
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ["Houve um erro, por favor tente mais tarde."] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
 };
 
 module.exports = {
