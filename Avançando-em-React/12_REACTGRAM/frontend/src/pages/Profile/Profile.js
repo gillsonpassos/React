0a42ac46897e349771b4220a72764b76969a6dc7
gillsonpassos <oliveirapassos07@outlook.com>
2023-06-16 15:17:29 -0300

    Formulario de postar foto

--- Avançando-em-React/12_REACTGRAM/frontend/src/pages/Profile/Profile.js
+++ Avançando-em-React/12_REACTGRAM/frontend/src/pages/Profile/Profile.js
@@ -23,12 +23,18 @@ export const Profile = () => {
   const { user, loading } = useSelector((state) => state.user);
   const { user: userAuth } = useSelector((state) => state.auth);
 
-  // photo
+  // New form and edit form refs
+  const newPhotoForm = useRef();
+  const editPhotoForm = useRef();
 
   // load user data
   useEffect(() => {
     dispatch(getUserDetails(id));
   }, [dispatch, id]);
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+  };
 
   if (loading) {
     return <p>Carregando...</p>;
@@ -45,6 +51,24 @@ export const Profile = () => {
           <p>{user.bio}</p>
         </div>
       </div>
+      {id === userAuth._id && (
+        <>
+          <div className="new-photo" ref={newPhotoForm}>
+            <h3>Compartilhe algum momenmto seu:</h3>
+            <form onSubmit={submitHandle}>
+              <label>
+                <span>Titulo para foto:</span>
+                <input type="text" placeholder="Insira um titulo" />
+              </label>
+              <label>
+                <span>Imagem:</span>
+                <input type="file" />
+              </label>
+              <input type="submit" value="Postar" />
+            </form>
+          </div>
+        </>
+      )}
     </div>
   );
 };
