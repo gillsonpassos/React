45d8ad7472d3d0b5bec9b8cbef3b36b505730f11
gillsonpassos <oliveirapassos07@outlook.com>
2023-06-26 11:38:45 -0300

    Service de resgatar foto do usuário

--- Avançando-em-React/12_REACTGRAM/frontend/src/pages/Profile/Profile.js
+++ Avançando-em-React/12_REACTGRAM/frontend/src/pages/Profile/Profile.js
@@ -14,6 +14,7 @@ import { useParams } from "react-router-dom";
 
 // redux
 import { getUserDetails } from "../../slices/userSlice";
+import { publishPhoto, resetMessage } from "../../slices/photoSlice";
 
 export const Profile = () => {
   const { id } = useParams();
@@ -22,6 +23,15 @@ export const Profile = () => {
 
   const { user, loading } = useSelector((state) => state.user);
   const { user: userAuth } = useSelector((state) => state.auth);
+  const {
+    photos,
+    loading: loadingPhoto,
+    message: messagePhoto,
+    error: errorPhoto,
+  } = useSelector((state) => state.photo);
+
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
 
   // New form and edit form refs
   const newPhotoForm = useRef();
@@ -32,8 +42,33 @@ export const Profile = () => {
     dispatch(getUserDetails(id));
   }, [dispatch, id]);
 
+  const handleFile = (e) => {
+    // image preview
+    const image = e.target.files[0];
+
+    setImage(image);
+  };
+
   const submitHandle = (e) => {
     e.preventDefault();
+
+    const photoData = {
+      title,
+      image,
+    };
+
+    // build form data
+    const formData = new FormData();
+
+    const photoFormData = Object.keys(photoData).forEach((key) =>
+      formData.append(key, photoData[key])
+    );
+
+    formData.append("photo", photoFormData);
+
+    dispatch(publishPhoto(formData));
+
+    setTitle("");
   };
 
   if (loading) {
@@ -58,15 +93,25 @@ export const Profile = () => {
             <form onSubmit={submitHandle}>
               <label>
                 <span>Titulo para foto:</span>
-                <input type="text" placeholder="Insira um titulo" />
+                <input
+                  type="text"
+                  placeholder="Insira um titulo"
+                  onChange={(e) => setTitle(e.target.value)}
+                  value={title || ""}
+                />
               </label>
               <label>
                 <span>Imagem:</span>
-                <input type="file" />
+                <input type="file" onChange={handleFile} />
               </label>
-              <input type="submit" value="Postar" />
+              {!loadingPhoto && <input type="submit" value="Postar" />}
+              {loadingPhoto && (
+                <input type="submit" disabled value="Aguarde..." />
+              )}
             </form>
           </div>
+          {errorPhoto && <Message msg={errorPhoto} type="error" />}
+          {messagePhoto && <Message msg={messagePhoto} type="succes" />}
         </>
       )}
     </div>
